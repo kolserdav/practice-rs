2a49abff9a2e7c51cde8972b1c02ee5777300c12
kolserdav <serega12101983@gmail.com>
2023-03-27 14:02:36 +0700

    lock closure

--- src/lock_closure.rs
+++ src/lock_closure.rs
@@ -1,3 +1,40 @@
+use once_cell::sync::Lazy;
+use std::{
+    sync::{Arc, Mutex},
+    thread::{sleep, spawn},
+    time::Duration,
+};
+
+struct Clos {
+    start: String,
+}
+
+impl Clos {
+    fn with_closure<F>(&self, cb: F)
+    where
+        F: Fn(String) + Send + 'static,
+    {
+        spawn(move || loop {
+            sleep(Duration::from_secs(2));
+            cb("call closure".to_string());
+        });
+    }
+}
+
+static CONN: Lazy<Arc<Mutex<Clos>>> = Lazy::new(|| {
+    Arc::new(Mutex::new(Clos {
+        start: String::from("start"),
+    }))
+});
+
 pub fn lock_closure() {
-    println!("lock closure");
+    let conn = CONN.lock().unwrap();
+
+    conn.with_closure(|m| {
+        let conn = CONN.clone();
+        let conn = conn.lock().unwrap();
+        println!("{}: {}", &conn.start, m);
+    });
+    drop(conn);
+    loop {}
 }
